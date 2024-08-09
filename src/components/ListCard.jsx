37abf4c2951e894a510aa84853513e5dc3564715
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-08-09 16:38:07 +0200

    Add modal

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import ModalContent from './ModalContent';
+
 export default function ListCard({ country }) {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <li className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform">
-      <h2 className="absolute top-0 left-0 p-2 bg-gray-50 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)] text-xl rounded">
-        {country.name.common}
-      </h2>
-      <img
-        className="w-full h-full object-cover rounded"
-        src={country.flags.svg}
-        alt="flag"
-      />
-    </li>
+    <>
+      <li
+        onClick={() => setShowModal(!showModal)}
+        className="relative cursor-pointer rounded transition-transform duration-300 hover:-translate-y-1 will-change-transform"
+      >
+        <h2 className="absolute top-0 left-0 p-2 bg-gray-50 drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)] text-xl rounded">
+          {country.name.common}
+        </h2>
+        <img
+          className="w-full h-full object-cover rounded"
+          src={country.flags.svg}
+          alt="flag"
+        />
+      </li>
+      {showModal &&
+        createPortal(
+          <ModalContent
+            country={country}
+            closeModal={() => setShowModal(!showModal)}
+          />,
+          document.body
+        )}
+    </>
   );
 }
